3cfb9398e97dffeb40b483bb320f4cb898a75d50
Guty <Facundo Gutirrez>
2016-01-19 00:20:09 -0300

    Modifiqué getPDF.py a la versión 1.2.5. El script en sí es el mismo, pero le agregué un mini manual de cosas que hay que hacer para que funcione.

--- getPDF.py
+++ getPDF.py
@@ -1,38 +1,78 @@
-#########################################################################
-#                                                                      	#
-#						getPDF version 1.2                             	#
-#																		#
-#########################################################################	   
-#                                                        				#
-#	Estado actual: (no pongo tildes porque arma quilombo)              	#
-#		En la carpeta donde se situa el script genera una carpeta con  	#
-#	el nombre "Live Archive" donde se comienzan a descargar todas las  	#
-#	regionales/world finals que hay en Live Archive en formato de pdf. 	#
-#	No se descargan aquellas regionales/world finals en las que el     	#
-#	boton "PDF" de Live Archive no funciona (incluso cuando hay un pdf 	#
-#	de menor calidad en la pagina del problema). Las regionales/ world 	#
-#	finals se guardan en carpetas y subcarpetas con sus respectivos    	#
-#	nombres adentro de la carpeta "Live Archive". Ademas de descargar  	#
-#	un pdf por cada por cada problema, por cada regional/world final se	#
-#	genera un pdf con todos los problemas de la prueba.                	#
-#	                                                                   	#
-#########################################################################	
-#                                                                      	#
-#	Futuras mejoras:                                                   	#
-#		* En aquellos casos donde no hay un pdf de alta calidad        	#
-#		pero existe un pdf de menor calidad, descargar este ultimo.    	#
-#		* En aquellos casos donde no hay pdf alguno documentar         	#
-#		debidamente el hecho ocurrido. Por ejemplo con un archivo de   	#
-#		texto/pdf que explique lo ocurrido.                             #
-#																	 	#
-#########################################################################                                                                      
+###################################################################################
+#                                                                      	          #
+#						getPDF version 1.2.5                           	          #
+#																		          #
+###################################################################################
+#                                                        				          #
+#	Estado actual: (no pongo tildes porque arma quilombo)              	          #
+#		En la carpeta donde se situa el script genera una carpeta con  	          #
+#	el nombre "Live Archive" donde se comienzan a descargar todas las  	          #
+#	regionales/world finals que hay en Live Archive en formato de pdf. 	          #
+#	No se descargan aquellas regionales/world finals en las que el     	          #
+#	boton "PDF" de Live Archive no funciona (incluso cuando hay un pdf 	          #
+#	de menor calidad en la pagina del problema). Las regionales/ world 	          #
+#	finals se guardan en carpetas y subcarpetas con sus respectivos    	          #
+#	nombres adentro de la carpeta "Live Archive". Ademas de descargar  	          #
+#	un pdf por cada por cada problema, por cada regional/world final se	          #
+#	genera un pdf con todos los problemas de la prueba.                	          #
+#	                                                                   	          #
+###################################################################################
+#                                                                      	          #
+#	Futuras mejoras:                                                   	          #
+#		* En aquellos casos donde no hay un pdf de alta calidad        	          #
+#		pero existe un pdf de menor calidad, descargar este ultimo.    	          #
+#		* En aquellos casos donde no hay pdf alguno documentar         	          #
+#		debidamente el hecho ocurrido. Por ejemplo con un archivo de   	          #
+#		texto que explique lo ocurrido.                                	          #
+#																	 	          #
+###################################################################################
+#                                                                                 #
+#	Mini Manual de Usuario:                                                       #
+#                                                                                 #
+#		1) Primero que nada hay que descargar 3 paquetes. Esto se puede           #
+#                                                                                 #
+#	hacer bastante facil si primero descargamos "pip", para lo cual               #
+#	hay que poner en la terminal "sudo apt-get install python-pip".               #
+#                                                                                 #
+#		2) Instalar los 3 paquetes que mencionamos. Para esto ponemos             #
+#                                                                                 #
+#	en la terminal:                                                               #
+#		"sudo pip install beautifulsoup4"                                         #
+#		"sudo pip install requests"                                               #
+#		"sudo pip install pyPDF2"                                                 #
+#                                                                                 #
+#		3) En: "usr/local/lib/python2.7/dist-packages/PyPDF2" hay que             #
+#                                                                                 #
+#		cambiar los permisos del arvhivo "generic.py" haciendo:                   #
+#	"sudo chmod 777 /usr/local/lib/python2.7/dist-packages/PyPDF2/generic.py"     #
+#                                                                                 #
+#		4) En "generic.py" hay que modificar la línea 576 donde dice:             #
+#                                                                                 #
+#		if not data.get(key):                                                     #
+#			data[key] = value                                                     #
+#		elif pdf.strict:                                                          #
+#			# multiple definitions of key not permitted                           #
+#			raise utils.PdfReadError("Multiple definitions in dictionary ...      #
+#		else:                                                                     #
+#			warnings.warn("Multiple definitions in dictionary ...                 #
+#                                                                                 #
+#		y poner:                                                                  #
+#                                                                                 #
+#		if data.has_key(key):                                                     #
+#			pass                                                                  #
+#		else:                                                                     #
+#			data[key] = value                                                     #
+#                                                                                 #
+#		5) Correr el script normalmente.                                          #
+#		                                                                          #
+###################################################################################                                                                   
 
 
 
 import requests,os,bs4
 from PyPDF2 import PdfFileMerger
 
-# Hay que instalar los paquetitos "PyPDF2", "requests" y "bs4". Se pueden instalar con: "sudo pip install xxxx" (con xxxx = nombre del paquetito)
+
 	
 requests.packages.urllib3.disable_warnings() # Esto hace falta en mi maquina, no se si es necesario siempre.
 rootUrl = 'https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&Itemid=8'
