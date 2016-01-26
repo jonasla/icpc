3532a6f512947e079e37d0cae6635242fd882c68
Guty <Facundo Gutirrez>
2016-01-26 16:24:12 -0300

    Agrego código (AC) de P-Networks de la Regional de South America 2005. Además lo quito de pendientes.txt

--- simulacros/Regionals 2005 - Latin America - South America/3475.cpp
+++ simulacros/Regionals 2005 - Latin America - South America/3475.cpp
@@ -2,29 +2,59 @@
 #include <deque>
 #include <vector>
 #include <set>
+#include <algorithm>
 
-#define forn(i,n) for(int i = 0; i < (int) (n);i++)
+typedef long long tint;
+#define forn(i,n) for(tint i=0;i<(int)(n);i++)
+#define forsn(i,s,n) for(tint i=s;i<(int)(n);i++)
 
 using namespace std;
 
-int main(){
-	int n,tmp,strokes;
-	cin >> n;
-	int strokes = 0;
-	set<int> a;
-	int b[n],v[n];
-	forn(i,n){
-		cin >> tmp;
-		a.insert(tmp);
-		cin >> b[i];
-	} 
-	if(a.count() < n){
-		cout << "No solution";
-	}else{
-		strokes = 0;
-		while(strokes < 4*n*n){
-			forn(i,n) v[i]=abs(b[i]-i);
-			
+int main()
+{
+	tint n;
+	while (cin >> n && n != 0)
+	{
+		vector<bool> used (n+1,false); // indexo de 1
+		vector<tint> perm (n+1); // indexo de 1
+		forsn(i,1,n+1)
+		{
+			tint p;
+			cin >> p;
+			perm[p] = i;
+			used[p] = true;
+		}
+		bool flag = false;
+		forsn(i,1,n+1)
+			if (!used[i])
+			{
+				cout << "No solution" << endl;
+				flag = true;
+				break;
+			}
+		if (!flag)
+		{
+			vector<tint> ans;
+			forsn(i,1,n+1)
+			{
+				while (perm[i] != i)
+				{
+					forsn(j,i,n+1)
+					{
+						if (perm[j] == i && j > 1)
+						{
+							swap(perm[j],perm[j-1]);
+							ans.push_back(j-1);
+						}
+					}
+				}
+
+			}
+			reverse(ans.begin(),ans.end());
+			cout << ans.size();
+			for(auto x : ans)
+				cout << " " << x;
+			cout << endl;
 		}
 	}
 	return 0;
