119ace27fbb2486699c37cec1a0a1a3f80baa8dd
Jonás <jonaslevy5@gmail.com>
2016-03-27 20:49:30 -0300

    Problema 6885 (Flowery Trails) Regionals 2014 - Europe - Southwestern: ACCEPTED.

--- simulacros/Regionals 2014 - Europe -Southwestern/85.cpp
+++ simulacros/Regionals 2014 - Europe -Southwestern/85.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <map>
+#include <set>
 
 using namespace std;
 
@@ -11,72 +12,54 @@ using namespace std;
 #define pb push_back
 const int INF = 1000000000;
 
-struct arista {
-	int peso, v1, v2, id, cant;
-	arista(int p, int u, int v, int i, int c) : peso(p), v1(u), v2(v), id(i), cant(c) { }
-};
-
-bool operator < (arista a1, arista a2)
-{
-	return mp(a1.peso, mp(a1.v1,a1.v2)) > mp(a2.peso,mp(a2.v1,a2.v2));
-}
-
 typedef vector<int> vi;
 typedef pair<int,int> ii;
+typedef vector<ii> vii;
 
-void dijk(vector<vi> &graf, int s, vi &dist, vi &padre, vector<arista> &aristas){
+void dijk(vector<vii> &graf, int s, vi &dist, vi &padre){
+	set<ii> Q;	// (w, ady)
 	dist[s] = 0;
-	padre[s] = -1;
-	priority_queue<arista> Q;
-	for(auto &ady : graf[s]){
-		Q.push( aristas[ady] );
-	}
+	Q.insert(mp(0,s));
 	while(!Q.empty()){
-		arista top = Q.top();
-		Q.pop();
-		int u = top.v1;
-		int v = top.v2;
-		int w = top.peso;
-		int id = top.id;
-		
-		if(dist[v] == INF || dist[u] + w <= dist[v] ){
-			dist[v] = dist[u] + w;
-			padre[v] = id;
-			for(auto &ady : graf[v]){
-				Q.push( aristas[ady] );
+		int cur = Q.begin()->second;
+		for(auto &ady : graf[cur]){
+			int next = ady.first;
+			int w = ady.second;
+			if(dist[next] > dist[cur] + w){
+				if(dist[next] != INF) Q.erase(mp(dist[next], next));
+				dist[next] = dist[cur] + w;
+				padre[next] = cur;
+				Q.insert(mp(dist[next], next));
 			}
 		}
+		Q.erase(mp(dist[cur],cur));
 	}
-}
-
-void printArista(arista &ar){
-	cout << ar.v1 << " -> " << ar.v2 << " " << ar.peso << " (" << ar.cant << ") " << ar.id << endl;
 }
 
 int main(){
+	ios_base::sync_with_stdio(0);
+
 	int N, M; 
 	while(cin >> N >> M && N != 0){
-		/// O(M log M)
-		map<ii, ii> pesos;
-		map<ii, int> cants;
 		
+		/// O(M log M)
+		// (u,v) -> (w,cantidad)
+		// u < v
+		map<ii, ii> aristas;
 		forn(i,M){
 			int u, v, w; cin >> u >> v >> w;
 			if(u == v) continue;
 			ii par = mp(min(u,v),max(u,v));
-			auto it = pesos.find(par);
+			auto it = aristas.find(par);
 			
-			if(it == pesos.end()){
-				pesos[par] = mp(w,1);
-				//pesos[mp(v,u)] = mp(w,1);
+			if(it == aristas.end()){
+				aristas[par] = mp(w,1);
 			}else{
 				if(w < (*it).second.first){
-					pesos[par] = mp(w,1);
-					//pesos[mp(v,u)] = mp(w,1);
+					aristas[par] = mp(w,1);
 				}else if(w == (*it).second.first){
 					int cant = (*it).second.second;
-					pesos[par] = mp(w,cant+1);
-					//pesos[mp(v,u)] = mp(w,cant+1);
+					aristas[par] = mp(w,cant+1);
 				}
 			}
 		}
@@ -85,78 +68,52 @@ int main(){
 		// minimo, guardamos la cantidad. 
 		
 		/// O(M)
-		vector<vi> graf(N, vi());
-		vector<arista> aristas;
-		
-		for(auto &it : pesos){
-			int u, v, w, id, cant;
-			u = it.first.first;
-			v = it.first.second;
-			w = it.second.first;
-			cant = it.second.second;
+		vector<vii> graf(N, vii());
+		for(auto &ar : aristas){
+			int u, v, w;
+			u = ar.first.first;
+			v = ar.first.second;
+			w = ar.second.first;
 			
-			id = aristas.size();
-			arista ar1 = arista(w, u, v, id, cant);
-			graf[u].pb(id);
-			aristas.pb(ar1);
-			
-			id = aristas.size();
-			arista ar2 = arista(w, v, u, id, cant);
-			graf[v].pb(id);
-			aristas.pb(ar2);
+			graf[u].pb(mp(v,w));
+			graf[v].pb(mp(u,w));
 		}
-				
-		/// O(dijkstra*2) + O(N)
-		vi distAB = vi(N, INF);
-		vi padreAB = vi(N, -1);
-		dijk(graf, 0, distAB, padreAB, aristas);
 		
-		vi distBA = vi(N, INF);
-		vi padreBA = vi(N, -1);
-		dijk(graf, N-1, distBA, padreBA, aristas);
+		/// O(2*dijk)
+		vi distAB(N, INF);
+		vi padreAB(N, -1);
+		dijk(graf, 0, distAB, padreAB);
 		
-		vector<bool> usoArista(aristas.size(), false);
+		vi distBA(N, INF);
+		vi padreBA(N, -1);
+		dijk(graf, N-1, distBA, padreBA);
+		
+		/// O(N log M)
+		set<ii> usoAristas;
 		int MINDIST = distAB[N-1];
 		forn(i,N){
 			if(distAB[i] + distBA[i] == MINDIST){
-				int v;
-				
-				v = i;
-				//while(padreAB[v] != -1){
-				if(padreAB[v] != -1){
-					usoArista[padreAB[v]] = true;
-					//v = aristas[padreAB[v]].v1;
+				if(padreAB[i] != -1){
+					int u = min(i,padreAB[i]);
+					int v = max(i,padreAB[i]);
+					usoAristas.insert(mp(u,v));
 				}
-				
-				v = i;
-				//while(padreBA[v] != -1){
-				if(padreBA[v] != -1){
-					usoArista[padreBA[v]] = true;
-					//v = aristas[padreBA[v]].v1;
+				if(padreBA[i] != -1){
+					int u = min(i,padreBA[i]);
+					int v = max(i,padreBA[i]);
+					usoAristas.insert(mp(u,v));
 				}
 			}
 		}
 		
 		/// O(M log M)
-		map<ii, ii> usoAristaBi;
-		forn(i,usoArista.size()){
-			if(usoArista[i]){
-				int u, v, w, cant;
-				u = aristas[i].v1;
-				v = aristas[i].v2;
-				w = aristas[i].peso;
-				cant = aristas[i].cant;
-				
-				usoAristaBi[mp(u,v)] = mp(w,cant);
-				usoAristaBi[mp(v,u)] = mp(w,cant);
-			}
-		}
-		/// O(M)
 		int RES = 0;
-		for(auto &ar : usoAristaBi){
-			RES += ar.second.first*ar.second.second;
+		for(auto &ar : usoAristas){
+			auto arC = aristas.find(ar);
+			RES += 2*(arC->second.first)*(arC->second.second);
 		}
 		cout << RES << endl;
 	}
+	
 	return 0;
 }
