00acd3caf04bc0aa69cb3b12c4dbcabfb2da9619
Jonás <jonaslevy5@gmail.com>
2016-04-02 14:26:50 -0300

    Renombro problema 3468

--- simulacros/Regionals 2005 - Latin America - South America/3468.cpp
+++ simulacros/Regionals 2005 - Latin America - South America/3468.cpp
@@ -1,62 +1,51 @@
 #include <iostream>
 #include <deque>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <valarray>
+
+typedef long long tint;
+#define forn(i,n) for(tint i=0;i<(int)(n);i++)
+#define forsn(i,s,n) for(tint i=s;i<(int)(n);i++)
 
 using namespace std;
 
-void printDeq(deque<bool> &vec){
-	for(unsigned int i=0; i<vec.size(); i++){
-		if(i) cout << " ";
-		cout << vec[i];
-	}
-	cout << endl;
-}
-
-int main(){
-
-	int n;
-	cin >> n;
-	while(n != 0){
-		deque<bool> postes;
-		
-		for(int i=0; i<n; i++){
-			int e; cin >> e;
-			postes.push_back((bool)e);
+int main()
+{
+	tint n;
+	while (cin >> n && n != 0)
+	{
+		valarray<bool> post (n);
+		tint q = 0, ans = 0, suma = 0;
+		forn(i,n)
+		{
+			cin >> post[i];
+			suma += post[i];
 		}
-		
-		//shifting
-		int s = 0;
-		while(!postes[n-1] && s < n){
-			//cout << "shift\n";
-			bool ult = postes[n-1];
-			postes.pop_back();
-			postes.push_front(ult);
-			
-			s++;
-		}
-		
-		//printDeq(postes);
-		
-		int ps = 0;
-		int cerosCon = 0;
-		for(unsigned int i=0; i<postes.size(); i++){
-			if(postes[i]){		// 1
-				if(cerosCon > 0){
-					ps += cerosCon / 2;
+		forn(i,n)
+			if (post[i])
+				{
+					post = post.cshift(i);
+					break;
 				}
-				cerosCon = 0;
-			}else{				// 0
-				cerosCon++;
+		if (suma == 0)
+			cout << (n+1)/2 << endl;
+		else
+		{
+			for(auto p : post)
+			{
+				if (!p)
+					q++;
+				else
+				{
+					ans += q/2;
+					q = 0;
+				}
 			}
+			cout << ans + q/2 << endl;
 		}
-		// todos 0
-		if(!postes[n-1]){
-			ps += cerosCon / 2;
-		}
-		
-		cout << ps << endl;
-		
-		cin >> n;
 	}
-	
 	return 0;
 }
+
